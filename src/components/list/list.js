bc4ea36e8df1140ee0357d92fa512f26d507aba8
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-01-19 22:23:20 -0800

    added row to method

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -17,23 +17,28 @@ class List extends Component{
 			})
 		})
 	}
+	createRows(){
+		return this.state.listData.map( (item, index) =>
+			<div className="row">
+				<img className="smallImage" src="images/tophat.jpg"/>
+				<div className="nameContainer">Top Hat</div>
+				<div className="priceContainer">$16.20</div>
+				<div className="colorContainer">
+					<div className="colorSquare" style={{backgroundColor: 'black'}}></div>
+					<div className="colorSquare" style={{backgroundColor: 'blue'}}></div>
+					<div className="colorSquare" style={{backgroundColor: 'red'}}></div>
+					<div className="colorSquare" style={{backgroundColor: 'grey'}}></div>
+				</div>
+				<div className="actionContainer">
+					<div className="action">+</div>
+				</div>
+			</div>			
+		)
+	}
 	render(){
 		return(
 			<div className="container">
-				<div className="row">
-					<img className="smallImage" src="images/tophat.jpg"/>
-					<div className="nameContainer">Top Hat</div>
-					<div className="priceContainer">$16.20</div>
-					<div className="colorContainer">
-						<div className="colorSquare" style={{backgroundColor: 'black'}}></div>
-						<div className="colorSquare" style={{backgroundColor: 'blue'}}></div>
-						<div className="colorSquare" style={{backgroundColor: 'red'}}></div>
-						<div className="colorSquare" style={{backgroundColor: 'grey'}}></div>
-					</div>
-					<div className="actionContainer">
-						<div className="action">+</div>
-					</div>
-				</div>
+
 				<div className="row">
 					<img className="smallImage" src="images/tophat.jpg"/>
 					<div className="nameContainer">Top Hat</div>
