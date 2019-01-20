01ac322879cb0fce74590297e376e90fc1a21b5b
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-01-19 20:31:53 -0800

    added skeleton and css to component

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -6,7 +6,39 @@ class List extends Component{
 		super(props);
 	}
 	render(){
-		return(<div>list</div>)
+		return(
+			<div class="container">
+					<div class="row">
+						<img class="smallImage" src="images/tophat.jpg">
+						<div class="nameContainer">Top Hat</div>
+						<div class="priceContainer">$16.20</div>
+						<div class="colorContainer">
+							<div class="colorSquare" style="background-color: black"></div>
+							<div class="colorSquare" style="background-color: blue"></div>
+							<div class="colorSquare" style="background-color: red"></div>
+							<div class="colorSquare" style="background-color: grey"></div>
+						</div>
+						<div class="actionContainer">
+							<div class="action">+</div>
+						</div>
+					</div>
+					<div class="row">
+						<img class="smallImage" src="images/tophat.jpg">
+						<div class="nameContainer">Top Hat</div>
+						<div class="priceContainer">$16.20</div>
+						<div class="colorContainer">
+							<div class="colorSquare" style="background-color: black"></div>
+							<div class="colorSquare" style="background-color: blue"></div>
+							<div class="colorSquare" style="background-color: red"></div>
+							<div class="colorSquare" style="background-color: grey"></div>
+						</div>
+						<div class="actionContainer">
+							<div class="action">+</div>
+						</div>
+					</div>
+				</div
+			</div>
+		)
 	}
 }
 
