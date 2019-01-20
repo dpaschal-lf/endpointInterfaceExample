f309e90b2d2bd383ba4b1c841f4ffbf97b60f359
Daniel Paschal <daniel.paschal@learningfuze.com>
2019-01-19 20:16:32 -0800

    initial commits

---
+++ src/components/list/list.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './list.css';
+
+class List extends Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		return(<div>list</div>)
+	}
+}
+
+export default List;
